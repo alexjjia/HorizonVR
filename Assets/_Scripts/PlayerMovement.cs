294141528fbcc04b9d561593461c1a31f4121d1d
Alex Jia <jayjialex@gmail.com>
2016-04-21 06:31:53 -0400

    Initial Commit
    Base version. I can see alot of different ways I can further implement
    this; it's good, because I can experiment as much as I want! :-)

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public static int playerSpeed = 10;
+    // FixedUpdate is called once per frame
+    void FixedUpdate()
+    {
+        gameObject.transform.Translate(Vector3.right * playerSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/_Scripts/SetSunLight.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetSunLight : MonoBehaviour
+{
+     
+
+    Material sky;
+
+    public Transform stars;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        sky = RenderSettings.skybox;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        stars.transform.rotation = transform.rotation;
+
+
+        Vector3 tvec = Camera.main.transform.position;
+    }
+}
